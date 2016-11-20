62f017bb16b5ec3539e47cf5128d837d1696bb7c
jcmayoral <jocamaba1989@gmail.com>
2016-11-20 21:13:20 +0100

    Revert "motion model"

--- Assignment/06/code/gaussian.py
+++ Assignment/06/code/gaussian.py
@@ -10,39 +10,9 @@ from matplotlib import pyplot as plt
 from pyexcel_ods import get_data
 from scipy.stats import norm
 from matplotlib.mlab import PCA
-from scipy import optimize
 
-
-def gaussian_func(height, center_x, center_y, width_x, width_y):
-    """Returns a gaussian function with the given parameters"""
-    width_x = float(width_x)
-    width_y = float(width_y)
-    return lambda x,y: height*np.exp(
-                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
-
-def moments(data):
-    """Returns (height, x, y, width_x, width_y)
-    the gaussian parameters of a 2D distribution by calculating its
-    moments """
-    total = data.sum()
-    X, Y = np.indices(data.shape)
-    x = (X*data).sum()/total
-    y = (Y*data).sum()/total
-    col = data[:, int(y)]
-    width_x = np.sqrt(np.abs((np.arange(col.size)-y)**2*col).sum()/col.sum())
-    row = data[int(x), :]
-    width_y = np.sqrt(np.abs((np.arange(row.size)-x)**2*row).sum()/row.sum())
-    height = data.max()
-    return height, x, y, width_x, width_y
-
-def fitgaussian(data):
-    """Returns (height, x, y, width_x, width_y)
-    the gaussian parameters of a 2D distribution found by a fit"""
-    params = moments(data)
-    errorfunction = lambda p: np.ravel(gaussian_func(*p)(*np.indices(data.shape)) -
-                                 data)
-    p, success = optimize.leastsq(errorfunction, params)
-    return p
+def gaussian(x, mu, sig):
+    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 
                                  
@@ -68,40 +38,40 @@ def plot_hist(data,nbins):
     #plt.show()
     #plt.savefig(path)
 
-def do_pca(data,index):
+def do_pca(data):
     pcaObject = PCA(data)
     #centered_data = pcaObject.center(data)
     projected_data = pcaObject.project(data)
     #print projected_data
-    plot_hist(projected_data[:,index],8)
+    plot_hist(projected_data[:,1],8)
     plt.show()
     
-if __name__ == "__main__":
-    #convert from ordered dict to lists
-    straight_motion_data =  get_data("../measurements2/second_see_data_original.ods",start_row=5,
-                                     row_limit=40,start_column=21,column_limit=4)
-                                     
-    data = straight_motion_data.get("Sheet1")
-    #dataMatrix = np.array(data)
-    
-    data_front_x = list()
-    data_front_y = list()
-    data_back_x = list()
-    data_back_y = list()
-    for x in range(0,40):
-        temp = data[x]
-        data_front_x.append(temp[0])
-        data_front_y.append(temp[1])
-        data_back_x.append(temp[2])
-        data_back_y.append(temp[3])
-    
-    robot_center_x = np.asarray(data_back_x) + (( np.asarray(data_front_x) -  np.asarray(data_back_x))/2)
-    robot_center_y = np.asarray(data_back_y) + (( np.asarray(data_front_y) -  np.asarray(data_back_y))/2)
-    robot_pose_angular = np.round(np.degrees(np.arctan2(robot_center_x,robot_center_y)),2)
-    robot_pose_linear = np.round(np.sqrt(robot_center_x*robot_center_x + robot_center_y*robot_center_y),2)
-    
-    data = np.c_[robot_pose_linear,robot_pose_angular]
-    do_pca(data,0)
+
+#convert from ordered dict to lists
+straight_motion_data =  get_data("../measurements2/second_see_data_original.ods",start_row=5,
+                                 row_limit=40,start_column=21,column_limit=4)
+                                 
+data = straight_motion_data.get("Sheet1")
+#dataMatrix = np.array(data)
+
+data_front_x = list()
+data_front_y = list()
+data_back_x = list()
+data_back_y = list()
+for x in range(0,40):
+    temp = data[x]
+    data_front_x.append(temp[0])
+    data_front_y.append(temp[1])
+    data_back_x.append(temp[2])
+    data_back_y.append(temp[3])
+
+robot_center_x = np.asarray(data_back_x) + (( np.asarray(data_front_x) -  np.asarray(data_back_x))/2)
+robot_center_y = np.asarray(data_back_y) + (( np.asarray(data_front_y) -  np.asarray(data_back_y))/2)
+robot_pose_angular = np.round(np.degrees(np.arctan2(robot_center_x,robot_center_y)),2)
+robot_pose_linear = np.round(np.sqrt(robot_center_x*robot_center_x + robot_center_y*robot_center_y),2)
+
+data = np.c_[robot_pose_linear,robot_pose_angular]
+do_pca(data)
 
 #path = "/home/chaitanya/MAS3/SEE-Project/Assignment/03/diagrams"
 #plot_hist(robot_center_y,4)
--- Assignment/07/motion model/motionmodel.py
+++ Assignment/07/motion model/motionmodel.py
@@ -34,69 +34,37 @@ for directory_name in directory_names:
     posesx.append(posex)
     posesy.append(posey)
     os.chdir('..')
-datax = []
-datay = []
+
 for x in range(0,len(posesx)):
     posex = posesx[x]
     posey = posesy[x]
-    datax = []
-    datay = []
+    
     for y in range(0,len(posex)):
         tempx = posex[y]
         tempy = posey[y]
+        meanx = np.mean(tempx)
+        stdx = np.std(tempx)
+        #centeredx = tempx - np.mean(tempx)
+        #centeredy = tempy - np.mean(tempy)
         
-        for j,k in zip(tempx,tempy):
-            datax.append(j)
-            datay.append(k)
-     
+        it = plt.scatter(tempx,tempy,color=colors[x],s=8)
+    figs.append(it)
 
-    meanx = np.mean(np.asanyarray(datax))
-    meany = np.mean(np.asanyarray(datay))
-    datax = np.array(datax)
-    datay = np.array(datay)
-    centeredx = np.subtract(datax,meanx)
-    centeredy = np.subtract(datay,meany)
-    centeredx = np.array(centeredx,dtype=np.double)
-    centeredy = np.array(centeredy,dtype=np.double)
-#    centeredx = [val - meanx for val in datax]
-#    centeredy = [val - meany for val in datay]
-    it = plt.scatter(centeredx,centeredy,color=colors[x],s=8)
-#        for valx,valy in zip(tempx,tempy):
-#                    datax.append(valx)
-#                    datay.append(valy)
-
-
-datax = np.asarray(datax)
-datay = np.asarray(datay)
-meanx = np.mean(datax)
-#stdx = np.std(tempx)
-meany = np.mean(datay)
-#stdy = np.std(tempy)
-#print meanx        
-#
-centeredx = np.subtract(tempx,meanx)
-centeredy = np.subtract(tempy,meany)
-
-plt.scatter(centeredx,centeredy)
-#it = plt.scatter(centeredx,centeredx,color=colors[x],s=8)
-##it = plt.scatter(tempx,tempy,color=colors[x],s=8)
-#figs.append(it)
-
-#print figs
-#plt.autoscale()
-#plt.grid('on')
-#plt.axis('equal')
-##ax = plt.gca()
-##plt.xlim(1000,-1000)
-##plt.ylim(200,-3000)
-#plt.xlabel('Y World Axis (in mm)')
-#plt.ylabel('X World Axis (in mm)')
-##plt.gca().set_aspect('equal', adjustable='box')
-#plt.legend((figs), 
-#   (directory_names),
-#   scatterpoints=1,
-#   ncol=1,
-#   loc='lower left',
-#   fontsize=16)
-#plt.legend()
-#plt.show()
+print figs
+plt.autoscale()
+plt.grid('on')
+plt.axis('equal')
+#ax = plt.gca()
+#plt.xlim(1000,-1000)
+#plt.ylim(200,-3000)
+plt.xlabel('Y World Axis (in mm)')
+plt.ylabel('X World Axis (in mm)')
+#plt.gca().set_aspect('equal', adjustable='box')
+plt.legend((figs), 
+   (directory_names),
+   scatterpoints=1,
+   ncol=1,
+   loc='lower left',
+   fontsize=16)
+plt.legend()
+plt.show()
